d91f35fa2fcce40e3cdda71e85d986e91f17df40
Arturo N III <arturo.norico.3@gmail.com>
2020-08-25 15:41:59 -0400

    reworking program
    reworking program to handle input parameters rather than rely on a global input array, which makes it easier to segment and specify what needs to be calculated

--- calculator/src/calculation/calculate.java
+++ calculator/src/calculation/calculate.java
@@ -1,24 +1,28 @@
 package calculation;
 
 public class calculate {
-    String[] inputArray;
+    String[] globalInArray;
     char[] pemdasArray = {'p','e','m','d','a','s'};
 
     public calculate(String inputString){
-        this.inputArray = inputString.split(" ");
+        this.globalInArray = inputString.split(" ");
     }
 
-    public int findSolution() {
+    public int startSolution() {
+        findSolution(globalInArray);
+        return Integer.valueOf(globalInArray[0]);
+    }
+
+    private void findSolution(String[] inputArray){
         for (int i = 1; i < pemdasArray.length; i++){
-            parseArray(pemdasArray[i]);
+            parseArray(pemdasArray[i],inputArray);
         }
-        return Integer.valueOf(inputArray[0]);
     }
 
-    private void parseArray(char symbol) {
+    private void parseArray(char symbol, String[] inputArray) {
         switch (symbol) {
             case 'p':
-                findParenthesis();
+                //findParenthesis();
                 break;
             case 'e':
                 doExponents();
@@ -40,20 +44,22 @@ public class calculate {
         }
     }
 
-    private void findParenthesis(){
+    private void findParenthesis(String[] inputArray){
         boolean repeat = true;
         int startLoc = -1;
         int endLoc = -1;
 
-        while(repeat == true){
-            for(int i = 0; i < inputArray.length; i++){
-                if (inputArray[i].compareTo("(") == 0){
+        while(repeat){
+            for(int i = 0; i < globalInArray.length; i++){
+                if (globalInArray[i].compareTo("(") == 0){
                     startLoc = i;
-                } else if (inputArray[i].compareTo(")") == 0){
+                } else if (globalInArray[i].compareTo(")") == 0){
                     endLoc = i;
                 }
                 if (startLoc > -1 && endLoc > -1){
-
+                    int result;
+                    shortenInputArray(0, startLoc,endLoc-startLoc);
+                    repeat = false;
                 }
             }
         }
@@ -64,12 +70,12 @@ public class calculate {
     private void doExponents(){
         boolean repeat = true;
         while(repeat){
-            for(int i = 0; i < inputArray.length; i++){
-                if (inputArray[i].compareTo("^")==0){
-                    int result = (int)Math.pow(Double.valueOf(inputArray[i-1]),Double.valueOf(inputArray[i+1]));
+            for(int i = 0; i < globalInArray.length; i++){
+                if (globalInArray[i].compareTo("^")==0){
+                    int result = (int)Math.pow(Double.valueOf(globalInArray[i-1]),Double.valueOf(globalInArray[i+1]));
                     shortenInputArray(result,i-1);
                 }
-                if (i == inputArray.length-1){
+                if (i == globalInArray.length-1){
                     repeat = false;
                 }
             }
@@ -79,13 +85,13 @@ public class calculate {
     private void doMultiplication(){
         boolean repeat = true;
         while(repeat){
-            for(int i = 0; i < inputArray.length; i++){
-                if (inputArray[i].compareTo("*")==0){
-                    int c = Integer.valueOf(inputArray[i-1]) * Integer.valueOf(inputArray[i+1]);
+            for(int i = 0; i < globalInArray.length; i++){
+                if (globalInArray[i].compareTo("*")==0){
+                    int c = Integer.valueOf(globalInArray[i-1]) * Integer.valueOf(globalInArray[i+1]);
                     shortenInputArray(c,i-1);
                     break;
                 }
-                if (i == inputArray.length-1){
+                if (i == globalInArray.length-1){
                     repeat = false;
                 }
             }
@@ -95,13 +101,13 @@ public class calculate {
     private void doDivision(){
         boolean repeat = true;
         while(repeat){
-            for(int i = 0; i < inputArray.length; i++){
-                if (inputArray[i].compareTo("/")==0){
-                    int c = Integer.valueOf(inputArray[i-1]) / Integer.valueOf(inputArray[i+1]);
+            for(int i = 0; i < globalInArray.length; i++){
+                if (globalInArray[i].compareTo("/")==0){
+                    int c = Integer.valueOf(globalInArray[i-1]) / Integer.valueOf(globalInArray[i+1]);
                     shortenInputArray(c,i-1);
                     break;
                 }
-                if (i == inputArray.length-1){
+                if (i == globalInArray.length-1){
                     repeat = false;
                 }
             }
@@ -111,13 +117,13 @@ public class calculate {
     private void doAddition(){
         boolean repeat = true;
         while(repeat){
-            for(int i = 0; i < inputArray.length; i++){
-                if (inputArray[i].compareTo("+")==0){
-                    int c = Integer.valueOf(inputArray[i-1]) + Integer.valueOf(inputArray[i+1]);
+            for(int i = 0; i < globalInArray.length; i++){
+                if (globalInArray[i].compareTo("+")==0){
+                    int c = Integer.valueOf(globalInArray[i-1]) + Integer.valueOf(globalInArray[i+1]);
                     shortenInputArray(c,i-1);
                     break;
                 }
-                if (i == inputArray.length-1){
+                if (i == globalInArray.length-1){
                     repeat = false;
                 }
             }
@@ -127,13 +133,13 @@ public class calculate {
     private void doSubtraction(){
         boolean repeat = true;
         while(repeat){
-            for(int i = 0; i < inputArray.length; i++){
-                if (inputArray[i].compareTo("-")==0){
-                    int c = Integer.valueOf(inputArray[i-1]) - Integer.valueOf(inputArray[i+1]);
+            for(int i = 0; i < globalInArray.length; i++){
+                if (globalInArray[i].compareTo("-")==0){
+                    int c = Integer.valueOf(globalInArray[i-1]) - Integer.valueOf(globalInArray[i+1]);
                     shortenInputArray(c,i-1);
                     break;
                 }
-                if (i == inputArray.length-1){
+                if (i == globalInArray.length-1){
                     repeat = false;
                 }
             }
@@ -142,17 +148,32 @@ public class calculate {
 
 
     private void shortenInputArray(int resultNumber, int replaceLocation){
-        String[] tempArray = new String[inputArray.length-2];
+        String[] tempArray = new String[globalInArray.length-2];
         int tempHold = 0;
         for (int i = 0; i < tempArray.length; i++){
             if (i == replaceLocation){
                 tempArray[i] = String.valueOf(resultNumber);
                 tempHold+=2;
             } else {
-                tempArray[i] = inputArray[tempHold];
+                tempArray[i] = globalInArray[tempHold];
                 tempHold++;
             }
         }
-        inputArray = tempArray;
+        globalInArray = tempArray;
+    }
+
+    private void shortenInputArray(int resultNumber, int replaceLocation, int shortenAmount){
+        String[] tempArray = new String[globalInArray.length-shortenAmount];
+        int tempHold = 0;
+        for (int i = 0; i < tempArray.length; i++){
+            if (i == replaceLocation){
+                tempArray[i] = String.valueOf(resultNumber);
+                tempHold+=shortenAmount;
+            } else {
+                tempArray[i] = globalInArray[tempHold];
+                tempHold++;
+            }
+        }
+        globalInArray = tempArray;
     }
 }
