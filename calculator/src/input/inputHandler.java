8d5bbe24640909f0382b562327fd20fb126c0ed4
Arturo N III <arturo.norico.3@gmail.com>
2020-11-13 17:15:27 -0500

    Addding input handling
    I am working on getting input handling taken care of so that the front end can be built and tested.

--- calculator/src/input/inputHandler.java
+++ calculator/src/input/inputHandler.java
@@ -1,5 +1,35 @@
 package input;
 
+import java.util.ArrayList;
+
 public class inputHandler {
 
+    ArrayList<String> formatedArray;
+    char[] potentialExpressions = {'(',')','*','/','+','-'};
+
+    public inputHandler(){
+    }
+
+    public void takeInput(String inputString){
+        formatedArray = new ArrayList<String>();
+        String tempChar = "";
+        for (int i = 0; i < inputString.length(); i++){
+            if (doesItContain(inputString.charAt(i),potentialExpressions)){
+                formatedArray.add(tempChar);
+                tempChar = "";
+                formatedArray.add(String.valueOf(inputString.charAt(i)));
+            } else {
+                tempChar += inputString.charAt(i);
+            }
+        }
+    }
+
+    public static boolean doesItContain(char toTest,char[] storeArray){
+        for (char temp:storeArray){
+            if (Character.compare(toTest, temp) == 0){
+                return true;
+            }
+        }
+        return false;
+    }
 }
